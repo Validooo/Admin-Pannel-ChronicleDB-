2ad36a2bb0d6f3d38cbbde3702b21942adab0496
Validooo <93727544+Validooo@users.noreply.github.com>
2022-04-17 16:52:09 +0200

    Add files via upload

---
+++ gulpfile.js
@@ -0,0 +1,72 @@
+var gulp       = require('gulp');
+var browserify = require('browserify');
+var watchify   = require('watchify');
+var source 		 = require("vinyl-source-stream");
+var babel      = require('gulp-babel');
+
+var watching = false;
+var demo     = false;
+
+
+gulp.task("default", ["prod"]);
+
+gulp.task("prod", function(){
+	gulp.src('./src/*.js')
+			.pipe(babel())
+			.pipe(gulp.dest('./lib'));
+	buildProdDist();
+});
+
+gulp.task("dev", function(){
+	watching = true;
+	buildDemoCode();
+	buildProdDist();
+});
+
+function buildDemoCode(){
+	demo = true;
+	browserifing("./example/js/amd-demo.js", "amd-demo.bundle.js", "./example/js");
+	browserifing("./example/js/browser-demo.js", "browser-demo.bundle.js", "./example/js");
+}
+
+function buildProdDist(){
+	demo = false;
+	browserifing("./src/index.js", "react-bootstrap-dropdown.min.js", "./dist");
+}
+
+function browserifing(main, bundleName, dest){
+	var b = browserify({
+	    entries: [main],
+			transform: ["babelify"],
+			cache: {},
+			debug: true,
+			packageCache: {},
+			fullPaths: true,
+	});
+
+	if(demo)
+		b = b.require(require.resolve('./src/Dropdown.js'),
+				{ expose: 'react-bootstrap-dropdown' });
+
+	if(watching){
+		b = watchify(b);
+		b.on('update', function(){
+			bundle(b, bundleName, dest);
+		});
+	}
+	bundle(b, bundleName, dest);
+}
+
+function bundle(b, bundleName, dest){
+	b.bundle()
+	.on('error', function(err){
+		console.log(err.message);
+		//this.end();
+  })
+	.on('end', function(){
+		console.log("building success.");
+		//this.end();
+	})
+	.pipe(source(bundleName))
+	.pipe(gulp.dest(dest));
+}
